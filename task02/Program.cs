33a5cb2730d84c012f36db5b2578e514f3fe873b
Пользователь <mintayeva01@mail.ru>
2022-12-11 15:31:26 +0600

    added second task

---
+++ task02/Program.cs
@@ -0,0 +1,14 @@
+﻿double x1 = 0;
+double x2 = 1;
+double y1 = 2;
+double y2 = 7;
+
+GetDistance(x1,x2,y1,y2);
+double result = GetDistance(x1,x2,y1,y2);
+static double GetDistance(double x1,double x2,double y1,double y2)
+{
+    return Math.Sqrt (Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+}
+
+Console.WriteLine (result);
+
